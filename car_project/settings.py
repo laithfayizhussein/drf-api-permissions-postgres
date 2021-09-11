243546b4d2291ecf432a124fad9eac5fadce776e
laithfayizhussein <laithalsanory9919@gmail.com>
2021-09-11 20:49:31 +0300

    have been fixed

--- car_project/settings.py
+++ car_project/settings.py
@@ -134,10 +134,10 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ]
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
